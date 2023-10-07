132321c8c85ed70ff13f4145281b1e61ccf11146
semih saçı <semihimes@yahoo.com>
2023-10-07 17:39:20 +0300

    Add files via upload

---
+++ examples/KarakterTest/KarakterTest.ino
@@ -0,0 +1,23 @@
+#include <Wire.h> 
+#include <Esp32_LiquidCrystalTr_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3f, 20, 4);
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  lcd.begin();
+
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+  lcd.print("BÜYÜK: İÜĞİŞÇÖ");
+  lcd.setCursor(0,1);
+  lcd.print("küçük: ıüğişçö");
+  delay(1000);
+  // Do nothing here...
+}
